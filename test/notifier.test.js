4e08a89ef749d907be8ab4489b9e409088e9bb98
tlueders <tanner.lueders@gmail.com>
2020-02-14 14:20:20 -0800

    updated tests to match new constructor config/errors

--- test/notifier.test.js
+++ test/notifier.test.js
@@ -6,20 +6,20 @@ describe('Notifier', () => {
     describe('#configure()', () => {
         it('provides missing config options', () => {
             assert.throws(() => {
-                new Notifier();
-            }, 'Notifier parameters are missing or type must be strings.')
+                new Notifier({api_key: '1234312'});
+            }, 'Required notifier parameters are missing or incorrect type')
         });
 
         it('provides incorrect type config options', () => {
             assert.throws(() => {
-                new Notifier({api_key: 'fail'});
-            }, 'Notifier parameters are missing or type must be strings.')
+                new Notifier('12314');
+            }, 'Notifier configuration must be an Object.')
         });
 
         it('checks for valid config options', () => {
-            let notifier = new Notifier('api-key', 'dev');
-            assert.equal('api-key', notifier.api_key);
-            assert.equal('dev', notifier.env);
+            let notifier = new Notifier({api_key: '1234312', env: 'production'});
+            assert.equal('1234312', notifier.config.api_key);
+            assert.equal('production', notifier.config.env);
         });
     });
 })
