ed8f28618f558cd1479cb0fefac8d8a93c0bfb4c
tlueders <tanner.lueders@gmail.com>
2020-02-14 10:52:00 -0800

    added error handling and tests for configuration

--- test/notifier.test.js
+++ test/notifier.test.js
@@ -1,12 +1,25 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
+var Notifier = require('../lib/main.js');
 
-var Notifier = require('../lib/main.js.js');
+describe('Notifier', () => {
+    describe('#configure()', () => {
+        it('provides missing config options', () => {
+            assert.throws(() => {
+                new Notifier();
+            }, 'Notifier parameters are missing or type must be strings.')
+        });
 
-describe('Notifier Placeholder Test', () => {
-    it('Should return Api Key', () => {
-        const notifier = new Notifier('1234');
-        const apiKey = notifier.getApiKey();
-        expect(apiKey).to.equals('1234');
-    })
-});
+        it('provides incorrect type config options', () => {
+            assert.throws(() => {
+                new Notifier({api_key: 'fail'});
+            }, 'Notifier parameters are missing or type must be strings.')
+        });
+
+        it('checks for valid config options', () => {
+            let notifier = new Notifier('api-key', 'dev');
+            assert.equal('api-key', notifier.api_key);
+            assert.equal('dev', notifier.env);
+        });
+    });
+})
